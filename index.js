af2cad4ab59461e3000b7a75efc4a259a478498d
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-06-18 08:52:41 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+  drivers.push(name)
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+  drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+  drivers.shift()
+}
+
+function appendDriver(name){
+  return copy = [...drivers,name]
+}
+
+function prependDriver(name){
+  return copy = [name,...drivers]
+}
+
+function removeLastDriver(){
+  return copy = drivers.slice(0,2)
+}
+
+function removeFirstDriver(){
+  return copy = drivers.slice(1)
+}
